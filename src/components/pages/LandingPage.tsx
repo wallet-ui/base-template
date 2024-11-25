328d43e4d44c159a8f845f5526db1e4da535ca99
Steven Sarmiento <sarmiento.steven@gmail.com>
2024-11-25 13:32:21 -0500

    fixing markdown styling for template

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -6,7 +6,6 @@ import { SelectedWalletAccountContext } from '@/context/SelectedWalletAccountCon
 import { AnimatedText } from '../ui/animated-text'
 import { IconSailboatFill } from 'symbols-react'
 import { CopyText } from '../ui/copy-text'
-import { Nav } from '../Nav'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
 
@@ -34,7 +33,6 @@ export function LandingPage() {
 
   return (
     <div className="flex flex-col items-center justify-center mx-auto px-4 text-center">
-       <Nav />
       <div className="inline-flex ring-1 ring-foreground/10 bg-foreground/5 rounded-2xl p-2 px-4 gap-2 mb-8">
         <IconSailboatFill className="w-[16px] h-[16px] fill-foreground/50" />
         <h2 className="text-[12px] line-height-[16px] font-semibold tracking-tight text-foreground/70 font-mono">
